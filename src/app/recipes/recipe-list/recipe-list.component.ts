3af053a07816d802d6db9d391e16bac23ea396dc
Gavishiddappa <gmgmadagi@gmail.com>
2017-05-18 00:02:59 +0530

    Shopping list Dropdown created

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe One', 'A Test may be', 'http://www.appsgeyser.com/assets/5df9921a/icons/recipe-icon-6.png'),
+    new Recipe('A Test Recipe Two', 'A Test may be', 'http://www.vivrebelle.com/assets/site/img/recipe-icon.png'),
+    new Recipe('A Test Recipe Three', 'A Test may be', 'https://assets.materialup.com/uploads/710e2dd7-6b50-43a0-a3f5-5167a7e23d07/teaser.png')
+  ];
 
   constructor() { }
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from './../shared/ingredient.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
 
   constructor() { }
 
